60dcc869fed4d93e4a5d44fe9787441df7e7185b
Marius <marius@priehl.de>
2018-04-20 18:41:01 +0200

    Testklasse vorbereitet

--- src/test/java/tdd/happyNumberCheckerTest.java
+++ src/test/java/tdd/happyNumberCheckerTest.java
@@ -1,6 +1,12 @@
 
 package tdd;
 
+import happyNumberChecker.checker;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
 public class happyNumberCheckerTest {
     
 }
